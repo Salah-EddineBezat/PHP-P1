08d23cf40214493f43ac493328001065fc697b56
Salah-EddineBezat <9025558@student.zadkine.nl>
2024-10-23 11:20:03 +0200

    Week-1

--- Week-1/index.php
+++ Week-1/index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Opdracht-1</title>
 </head>
-<body>
-    
+
+<h1>Start PHP</h1>
+
+<?php 
+echo "Hello world";
+?>
+
 </body>
 </html>
