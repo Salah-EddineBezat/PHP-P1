4ecacf14a123abc5e76b04b9f23a5eec3c18ad35
Salah-EddineBezat <9025558@student.zadkine.nl>
2024-11-05 09:06:44 +0100

    Opdracht_2
    Hierbij verstuur ik opdracht 2

--- Week-3/date.php
+++ Week-3/date.php
@@ -1,3 +1,24 @@
-<?php 
-echo "Vandaag is het: " .date("Y-m-d H:i:s");
+<?php
+$today = date ("l j F Y");
+echo " Vandaag is het: $today .";
+
+echo "<br>";
+
+$day = date (" z");
+echo " Vandaag is het de $day e  dag van het jaar.";
+
+echo "<br>";
+
+$dayOfTheWeek = date("l");
+$numericDay = date("w");
+echo "$dayOfTheWeek is de $numericDay e dag van de week.";
+
+echo "<br>";
+
+$month = date ("F");
+$daysInMonth = date ("t");
+echo "De maand , $month heeft in totaal $daysInMonth dagen."
+
+
+
 ?>
