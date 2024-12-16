232c7f52bfa9a0783af1f7650632c906a1ac3968
Salah-EddineBezat <9025558@student.zadkine.nl>
2024-12-16 22:01:42 +0100

    Aanpassing formulier validatie
    Hierbij heb ik de code aangepast op basis van de feedback.

--- Week-6/process_form.php
+++ Week-6/process_form.php
@@ -2,11 +2,11 @@
 $name = $message = $email = "";
 $nameError = $emailError = $messageError = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST['submit'])) {
   // Naam validatie //
   if (empty($_POST['name'])) {
     $nameError = "Het naamveld is verplicht!";
-  } elseif (strlen($_POST['name']) < 3) {
+  } elseif (strlen(trim($_POST['name'])) < 3) {
     $nameError = "De naam moet minimaal 3 tekens lang zijn.";
   } else {
     $name = htmlspecialchars($_POST['name']);
@@ -18,20 +18,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } elseif (!preg_match("/^[^@]+@[^@]+\.[^@]+$/", $_POST['email'])) {
     $emailError = "Het emailadres moet een '@' en een '.' bevatten.";
   } else {
-    $email = htmlspecialchars($_POST['email']);
+    $email = htmlspecialchars(trim($_POST['email']));
   }
 
   // Bericht validatie//
   if (empty($_POST['message'])) {
     $messageError = "Vul een bericht in!";
-  } elseif (strlen($_POST['message']) < 10) {
+  } elseif (strlen(trim($_POST['message'])) < 10) {
     $messageError = "Het bericht moet minimaal 10 tekens lang zijn.";
   } else {
-    $message = htmlspecialchars($_POST['message']);
+    $message = htmlspecialchars(trim($_POST['message']));
   }
 }
 
-if (empty($nameError) && empty($messageError) && empty($emailError)) {
+
+
+if(isset($_POST['submit']) && empty($nameError) && empty($messageError) && empty($emailError)) {
   echo "<h3> Ingevoerde gegevens:</h3>";
   echo "Naam: $name<br>";
   echo "E-mail: $email<br>";
